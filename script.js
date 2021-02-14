816805b001b893cc8b0c79be7939f98addb6727f
minuiuu <63951354+minuiuu@users.noreply.github.com>
2021-02-15 02:23:05 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,213 @@
+
+  $(".year11").mouseover(function(){
+    $(".hover-r11").css("background-color","#FF4A4A").css("color","white");
+  });$(".year11").mouseout(function(){
+    $(".hover-r11").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year11").mouseover(function(){
+    $(".hover-b11").css("background-color","#4A90FF").css("color","white");
+  });$(".year11").mouseout(function(){
+    $(".hover-b11").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year11").mouseover(function(){
+    $(".hover-g11").css("background-color","#1ACE6D").css("color","white");
+  });$(".year11").mouseout(function(){
+    $(".hover-g11").css("background-color","white").css("color","#383838");
+  });
+
+//2012 
+
+  $(".year12").mouseover(function(){
+    $(".hover-r12").css("background-color","#FF4A4A").css("color","white");
+  });$(".year12").mouseout(function(){
+    $(".hover-r12").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year12").mouseover(function(){
+    $(".hover-b12").css("background-color","#4A90FF").css("color","white");
+  });$(".year12").mouseout(function(){
+    $(".hover-b12").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year12").mouseover(function(){
+    $(".hover-g12").css("background-color","#1ACE6D").css("color","white");
+  });$(".year12").mouseout(function(){
+    $(".hover-g12").css("background-color","white").css("color","#383838");
+  });
+
+  //2013
+  $(".year13").mouseover(function(){
+    $(".hover-r13").css("background-color","#FF4A4A").css("color","white");
+  });$(".year13").mouseout(function(){
+    $(".hover-r13").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year13").mouseover(function(){
+    $(".hover-b13").css("background-color","#4A90FF").css("color","white");
+  });$(".year13").mouseout(function(){
+    $(".hover-b13").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year13").mouseover(function(){
+    $(".hover-g13").css("background-color","#1ACE6D").css("color","white");
+  });$(".year13").mouseout(function(){
+    $(".hover-g13").css("background-color","white").css("color","#383838");
+  });
+
+// 2014
+  $(".year14").mouseover(function(){
+    $(".hover-r14").css("background-color","#FF4A4A").css("color","white");
+  });$(".year14").mouseout(function(){
+    $(".hover-r14").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year14").mouseover(function(){
+    $(".hover-b14").css("background-color","#4A90FF").css("color","white");
+  });$(".year14").mouseout(function(){
+    $(".hover-b14").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year14").mouseover(function(){
+    $(".hover-g14").css("background-color","#1ACE6D").css("color","white");
+  });$(".year14").mouseout(function(){
+    $(".hover-g14").css("background-color","white").css("color","#383838");
+  });
+
+  //2015
+  $(".year15").mouseover(function(){
+    $(".hover-r15").css("background-color","#FF4A4A").css("color","white");
+  });$(".year15").mouseout(function(){
+    $(".hover-r15").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year15").mouseover(function(){
+    $(".hover-b15").css("background-color","#4A90FF").css("color","white");
+  });$(".year15").mouseout(function(){
+    $(".hover-b15").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year15").mouseover(function(){
+    $(".hover-g15").css("background-color","#1ACE6D").css("color","white");
+  });$(".year15").mouseout(function(){
+    $(".hover-g15").css("background-color","white").css("color","#383838");
+  });
+
+//2016
+  $(".year16").mouseover(function(){
+    $(".hover-r16").css("background-color","#FF4A4A").css("color","white");
+  });$(".year16").mouseout(function(){
+    $(".hover-r16").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year16").mouseover(function(){
+    $(".hover-b16").css("background-color","#4A90FF").css("color","white");
+  });$(".year16").mouseout(function(){
+    $(".hover-b16").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year16").mouseover(function(){
+    $(".hover-g16").css("background-color","#1ACE6D").css("color","white");
+  });$(".year16").mouseout(function(){
+    $(".hover-g16").css("background-color","white").css("color","#383838");
+  });
+
+//2017
+  $(".year17").mouseover(function(){
+    $(".hover-r17").css("background-color","#FF4A4A").css("color","white");
+  });$(".year17").mouseout(function(){
+    $(".hover-r17").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year17").mouseover(function(){
+    $(".hover-b17").css("background-color","#4A90FF").css("color","white");
+  });$(".year17").mouseout(function(){
+    $(".hover-b17").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year17").mouseover(function(){
+    $(".hover-g17").css("background-color","#1ACE6D").css("color","white");
+  });$(".year17").mouseout(function(){
+    $(".hover-g17").css("background-color","white").css("color","#383838");
+  });
+
+  //2018
+  $(".year18").mouseover(function(){
+    $(".hover-r18").css("background-color","#FF4A4A").css("color","white");
+  });$(".year18").mouseout(function(){
+    $(".hover-r18").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year18").mouseover(function(){
+    $(".hover-b18").css("background-color","#4A90FF").css("color","white");
+  });$(".year18").mouseout(function(){
+    $(".hover-b18").css("background-color","white").css("color","#383838");
+  });
+
+  $(".year18").mouseover(function(){
+    $(".hover-g18").css("background-color","#1ACE6D").css("color","white");
+  });$(".year18").mouseout(function(){
+    $(".hover-g18").css("background-color","white").css("color","#383838");
+  });
+
+  $(function() {
+    $("#g1, #g2").hover(function() {
+      $("#g0").addClass("hhh");
+    }, function() {
+      $("#g0").removeClass("hhh");
+    });
+  });
+
+  $(function() {
+    $("#g3, #g4").hover(function() {
+      $("#g0").addClass("hhh");
+    }, function() {
+      $("#g0").removeClass("hhh");
+    });
+  });
+
+  $(function() {
+    $("#g5, #g6").hover(function() {
+      $("#g0").addClass("hhh");
+    }, function() {
+      $("#g0").removeClass("hhh");
+    });
+  });
+
+
+  $(function() {
+    $("#g7, #g8").hover(function() {
+      $("#g0").addClass("hhh");
+    }, function() {
+      $("#g0").removeClass("hhh");
+    });
+  });
+
+
+  $(function() {
+    $("#g9, #g10").hover(function() {
+      $("#g0").addClass("hhh");
+    }, function() {
+      $("#g0").removeClass("hhh");
+    });
+  });
+
+  $(function() {
+    $("#g11, #g12").hover(function() {
+      $("#g0").addClass("hhh");
+    }, function() {
+      $("#g0").removeClass("hhh");
+    });
+  });
+
+  $(function() {
+    $("#g13, #g14").hover(function() {
+      $("#g0").addClass("hhh");
+    }, function() {
+      $("#g0").removeClass("hhh");
+    });
+  });
+
+
+
